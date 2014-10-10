6bf13e0f1d541dda43b6f5de73e7e36a19656b72
Jay Varner <jayvarner@gmail.com>
2014-10-10 10:46:26 -0700

    Changes to the modle for the new database structure [#66954860].

--- libraryuse/models.py
+++ libraryuse/models.py
@@ -10,7 +10,7 @@ class DataExport(models.Model):
 class LibraryVisitKey(models.Model):
     LIBRARY_VISIT_KEY_CHOICES = (
         ('visit_time', 'Time through the turnstile'),
-        ('term_number', 'Get term code definition from etd'),        
+        ('term_number', 'Get term code definition from etd'),
         ('prsn_i_pblc', 'Public identifier of the person  (PPID)'),
         ('prsn_i_ecn', 'EmoryCard number identifier of the person'),
         ('prsn_i_hr', 'HR (emplid) identifier of the person'),
@@ -42,21 +42,13 @@ class LibraryVisitKey(models.Model):
     key = models.CharField(max_length=30, choices=LIBRARY_VISIT_KEY_CHOICES)
 
 class LibraryVisit(models.Model):
-    #id = models.CharField(max_length=36L)
-    idnumber = models.CharField(max_length=20L)
-    lastname = models.CharField(max_length=80L)
-    firstname = models.CharField(max_length=80L)
-    prsn_i_pblc = models.CharField(max_length=8L, db_column='PRSN_I_PBLC', blank=True) #public identifier of person
-    prsn_i_ecn = models.CharField(max_length=9L, db_column='PRSN_I_ECN', blank=True) #EmoryCard number
+    prsn_i_pblc = models.CharField(max_length=32L, db_column='PRSN_I_PBLC', blank=True) #md5 hash of public identifier of person
+    prsn_i_ecn = models.CharField(max_length=32L, db_column='PRSN_I_ECN', blank=True) #md5 hash of EmoryCard number
     prsn_i_hr = models.CharField(max_length=7L, db_column='PRSN_I_HR', blank=True) #HR (emplid) of person
     prsn8hc_i_hr = models.CharField(max_length=7L, db_column='PRSN8HC_I_HR', blank=True) #Healthcare HR ID (emplid) of person
     prsn_i_sa = models.CharField(max_length=7L, db_column='PRSN_I_SA', blank=True) #SA (emplid) of the person
     prsn_e_titl_dtry = models.CharField(max_length=70L, db_column='PRSN_E_TITL_DTRY', blank=True) #directory title of person
-    
-    #anonymous
-    #term_date = models.DateTimeField() # time through turnstile
     visit_time = models.DateTimeField() #"normalized" to minute to minimize multiswipe entrances
-    term_number = models.CharField(max_length=80L)
     location = models.CharField(max_length=80L)
     prsn_c_type = models.CharField(max_length=1L, db_column='PRSN_C_TYPE', blank=True) #EU type code of the person
     prsn_e_type = models.CharField(max_length=30L, db_column='PRSN_E_TYPE', blank=True) #EU type description of the person
@@ -74,7 +66,7 @@ class LibraryVisit(models.Model):
     dvsn8hc_i = models.CharField(max_length=10L, db_column='DVSN8HC_I', blank=True) #HC division identifier of the employee job
     dvsn8hc_n = models.CharField(max_length=40L, db_column='DVSN8HC_N', blank=True) #HC division name of the employee job
     acca_i = models.CharField(max_length=4L, db_column='ACCA_I', blank=True) #Academic career identifier of the student (primary)
-    acpr_n = models.CharField(max_length=30L, db_column='ACPR_N', blank=True) #Academic program name of the student 
+    acpr_n = models.CharField(max_length=30L, db_column='ACPR_N', blank=True) #Academic program name of the student
     acpl_n = models.CharField(max_length=30L, db_column='ACPL_N', blank=True) #Academic plan name of the student
     stdn_e_clas = models.CharField(max_length=30L, db_column='STDN_E_CLAS', blank=True) #Class description of the student
     stdn_f_ungr = models.CharField(max_length=1L, db_column='STDN_F_UNGR', blank=True) #Undergraduate flag of the student
